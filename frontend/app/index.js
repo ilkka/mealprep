539b634e1aed1b8d2bae4e94c12e80c93b7138c7
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2015-11-16 00:15:36 +0200

    Load an ingredient
    Also whoops back to 2 spaces I guess

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,17 +1,30 @@
 import Rx from 'rx';
 import Cycle from '@cycle/core';
 import {makeDOMDriver, h} from '@cycle/dom';
+import {makeHTTPDriver} from '@cycle/http';
+
 import {ifOk, ifError, returnAsObservable} from './helpers/map-errors';
 
-function main({DOM}) {
-    return {
-        DOM: Rx.Observable.just(true)
-            .map(() => h('p', 'MOROOOOO')),
-    };
+const INGREDIENT_URL = 'http://localhost:4000/api/v1/ingredients/123';
+
+function main({DOM, HTTP}) {
+  let request$ = Rx.Observable.just(INGREDIENT_URL);
+  let vtree$ = HTTP.filter((res$) => res$.request === INGREDIENT_URL)
+        .mergeAll()
+        .map((req) => JSON.parse(req.text))
+        .map((ing) => ing.data.name)
+        .startWith('Loading...')
+        .map((ing) => h('p', ing));
+
+  return {
+    DOM: vtree$,
+    HTTP: request$,
+  };
 }
 
 Cycle.run(main, {
-    DOM: makeDOMDriver('#app', {
-        'labeled-slider': require('./components/labeled-slider'),
-    }),
+  DOM: makeDOMDriver('#app', {
+    'labeled-slider': require('./components/labeled-slider'),
+  }),
+  HTTP: makeHTTPDriver(),
 });
