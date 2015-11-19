1e552bfc516ede290d1d0328826ff1da8cb2ec6a
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2015-11-19 17:28:34 +0200

    Add the HTTP stuff back

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,16 +1,16 @@
 import Rx from 'rx';
 import Cycle from '@cycle/core';
 import {makeDOMDriver, h} from '@cycle/dom';
-//import {makeHTTPDriver} from '@cycle/http';
+import {makeHTTPDriver} from '@cycle/http';
 import labeledSlider from './components/labeled-slider/index.js';
 //import {ifOk, ifError, returnAsObservable} from './helpers/map-errors';
 
 const INGREDIENT_URL = 'http://localhost:4000/api/v1/ingredients/123';
 
 function view(state) {
-  return state.map(({amount}) => h('div', [
+  return state.map(({amount, ingredient}) => h('div', [
     h('labeled-slider#amount', {
-      key: 1, label: 'Amount', unit: 'g', min: 0, initial: amount, max: 100,
+      key: 1, label: ingredient, unit: 'g', min: 0, initial: amount, max: 100,
     }),
   ]));
 }
@@ -18,41 +18,27 @@ function view(state) {
 function model(actions) {
   return Rx.Observable.combineLatest(
     actions.changeAmount.startWith(50),
-    (amount) => ({amount}));
+    actions.changeIngredient.startWith('Loading...'),
+    (amount, ingredient) => ({amount, ingredient}));
 }
 
-function intent(DOM) {
+function intent({DOM, HTTP}) {
   return {
+    changeIngredient: HTTP.filter((res$) => res$.request === INGREDIENT_URL)
+      .mergeAll()
+      .map((req) => {
+        console.log(req);
+        return JSON.parse(req.text);
+      })
+      .map((ing) => ing.data.name),
     changeAmount: DOM.select('#amount').events('newValue').map((ev) => ev.detail),
   };
 }
 
-function main({DOM/*, HTTP*/}) {
-  // let request$ = Rx.Observable.just(INGREDIENT_URL);
-  //
-  // let vtree$ = HTTP.filter((res$) => res$.request === INGREDIENT_URL)
-  //       .mergeAll()
-  //       .map((req) => JSON.parse(req.text))
-  //       .map((ing) => ing.data.name)
-  //       .startWith('Loading...')
-  //       .map((ing) => h('div', [
-  //         h('p', ing),
-  //         h('labeled-slider#amount', {
-  //           key: 1,
-  //           label: 'Amount',
-  //           unit: 'g',
-  //           min: 0,
-  //           initial: 10,
-  //           max: 100,
-  //         }),
-  //       ]));
-  //
-  // return {
-  //   DOM: vtree$,
-  //   //HTTP: request$,
-  // };
+function main({DOM, HTTP}) {
   return {
-    DOM: view(model(intent(DOM))),
+    DOM: view(model(intent({DOM, HTTP}))),
+    HTTP: Rx.Observable.just(INGREDIENT_URL),
   };
 }
 
@@ -60,5 +46,5 @@ Cycle.run(main, {
   DOM: makeDOMDriver('#app', {
     'labeled-slider': labeledSlider,
   }),
-  //HTTP: makeHTTPDriver(),
+  HTTP: makeHTTPDriver(),
 });
