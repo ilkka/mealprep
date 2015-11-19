02ac682b0dcee5f647c271f7fd82ee60e8d5c497
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2015-11-19 21:24:43 +0200

    Hyperscript helpers

--- frontend/app/components/ingredient-details/index.js
+++ frontend/app/components/ingredient-details/index.js
@@ -2,6 +2,8 @@ import Rx from 'rx';
 import {h} from '@cycle/dom';
 import numeral from 'numeral';
 
+const {div, h2, h3, dl, dt, dd} = require('hyperscript-helpers')(h);
+
 export default function ingredientDetails(responses) {
   function intent(/*DOM*/) {
     return {};
@@ -19,12 +21,12 @@ export default function ingredientDetails(responses) {
   function view(state$) {
     return state$
       .filter(({ingredient, amount}) => ingredient.components)
-      .map(({ingredient, amount}) => h('div.ingredient', [
-        h('h2', `${ingredient.name}, ${amount} g`),
-        h('h3', 'Ravintotekijät:'),
-        h('dl', ingredient.components.reduce((l, c) => l.concat(
-          h('dt', `${c.name}`),
-          h('dd', `${numeral(c.value * (amount / 100.0)).format('0.00')} ${c.unit}`)
+      .map(({ingredient, amount}) => div('.ingredient', [
+        h2(`${ingredient.name}, ${amount} g`),
+        h3('Ravintotekijät:'),
+        dl(ingredient.components.reduce((l, c) => l.concat(
+          dt(`${c.name}`),
+          dd(`${numeral(c.value * (amount / 100.0)).format('0.00')} ${c.unit}`)
         ), [])),
       ]));
   }
