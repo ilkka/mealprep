27b09188075cafcfd25843c1fc3c12a0e2a23d3b
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2015-11-28 16:14:15 +0200

    Slight refactor to decrease nesting

--- frontend/app/components/ingredient-selector/index.js
+++ frontend/app/components/ingredient-selector/index.js
@@ -1,29 +1,29 @@
 import {h} from '@cycle/dom';
 const {input, label, div} = require('hyperscript-helpers')(h);
 
+function intent(DOM) {
+  return {
+    searchIngredientId$: DOM.select('#ingredientId')
+      .events('keydown')
+      .debounce(500)
+      .map((ev) => ev.target.value),
+  };
+}
+
+function model(actions) {
+  return actions.searchIngredientId$
+    .startWith('')
+    .map((ingredientId) => ({ingredientId}));
+}
+
+function view(state$) {
+  return state$.map(({id}) => div('.ingredient-selector', [
+    label('Ingredient ID'),
+    input('#ingredientId', {value: id}),
+  ]));
+}
+
 export default function ingredientSelector(responses) {
-  function intent(DOM) {
-    return {
-      searchIngredientId$: DOM.select('#ingredientId')
-        .events('keydown')
-        .debounce(500)
-        .map((ev) => ev.target.value),
-    };
-  }
-
-  function model(actions) {
-    return actions.searchIngredientId$
-      .startWith('')
-      .map((ingredientId) => ({ingredientId}));
-  }
-
-  function view(state$) {
-    return state$.map(({id}) => div('.ingredient-selector', [
-      label('Ingredient ID'),
-      input('#ingredientId', {value: id}),
-    ]));
-  }
-
   return {
     DOM: view(model(intent(responses.DOM))),
     events: {},
--- frontend/app/components/labeled-slider/index.js
+++ frontend/app/components/labeled-slider/index.js
@@ -1,34 +1,34 @@
 import Rx from 'rx';
 import {h} from '@cycle/dom';
 
+function intent(DOM) {
+  return {
+    changeValue$: DOM.select('.slider').events('input')
+      .map((ev) => ev.target.value),
+  };
+}
+
+function model(context, actions) {
+  let initialValue$ = context.props.get('initial').first();
+  let value$ = initialValue$.concat(actions.changeValue$);
+  let props$ = context.props.getAll();
+  return Rx.Observable.combineLatest(props$, value$,
+    (props, value) => { return {props, value}; }
+  );
+}
+
+function view(state$) {
+  return state$.map((state) => {
+    let {label, unit, min, max} = state.props;
+    let value = state.value;
+    return h('div.labeled-slider', [
+      h('span.label', [label + ' ' + value + unit]),
+      h('input.slider', {type: 'range', min, max, value}),
+    ]);
+  });
+}
+
 export default function labeledSlider(responses) {
-  function intent(DOM) {
-    return {
-      changeValue$: DOM.select('.slider').events('input')
-        .map((ev) => ev.target.value),
-    };
-  }
-
-  function model(context, actions) {
-    let initialValue$ = context.props.get('initial').first();
-    let value$ = initialValue$.concat(actions.changeValue$);
-    let props$ = context.props.getAll();
-    return Rx.Observable.combineLatest(props$, value$,
-      (props, value) => { return {props, value}; }
-    );
-  }
-
-  function view(state$) {
-    return state$.map((state) => {
-      let {label, unit, min, max} = state.props;
-      let value = state.value;
-      return h('div.labeled-slider', [
-        h('span.label', [label + ' ' + value + unit]),
-        h('input.slider', {type: 'range', min, max, value}),
-      ]);
-    });
-  }
-
   let actions = intent(responses.DOM);
   let vtree$ = view(model(responses, actions));
 
