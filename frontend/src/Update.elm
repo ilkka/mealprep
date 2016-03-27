a302c24295ea32a78e298f6c7f3a8a800c67bc6b
Ilkka Laukkanen <ilkka@ilkka.io>
2016-03-27 12:36:24 +0300

    Wire up routing into app update

--- frontend/src/Update.elm
+++ frontend/src/Update.elm
@@ -1,14 +1,16 @@
 module Update (..) where
 
 import Models exposing (..)
+import Debug
 import Actions exposing (..)
 import Effects exposing (Effects)
+import Routing
 import Meals.Update
 
 
 update : Action -> AppModel -> ( AppModel, Effects Action )
 update action model =
-  case action of
+  case (Debug.log "action" action) of
     MealsAction subAction ->
       let
         updateModel =
@@ -18,6 +20,13 @@ update action model =
           Meals.Update.update subAction updateModel
       in
         ( { model | meals = updatedMeals }, Effects.map MealsAction fx )
+
+    RoutingAction subAction ->
+      let
+        ( updatedRouting, fx ) =
+          Routing.update subAction model.routing
+      in
+        ( { model | routing = updatedRouting }, Effects.map RoutingAction fx )
 
     NoOp ->
       ( model, Effects.none )
