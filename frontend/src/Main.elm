3c6c60074ddb3016bd7a8e1592bd35a24601bbbb
Ilkka Laukkanen <ilkka@ilkka.io>
2016-06-05 18:36:20 +0300

    Nav and url parsing

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -2,8 +2,10 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.App as Html
 import Html.Events exposing (onClick)
+import Navigation
+import UrlParser exposing (Parser, (</>), oneOf, format, int)
+import String
 
 
 -- APP
@@ -11,7 +13,50 @@ import Html.Events exposing (onClick)
 
 main : Program Never
 main =
-    Html.beginnerProgram { model = model, view = view, update = update }
+    Navigation.program urlParser
+        { init = init
+        , update = update
+        , urlUpdate = urlUpdate
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- URL PARSERS
+
+
+urlParser : Navigation.Parser (Result String Page)
+urlParser =
+    Navigation.makeParser hashParser
+
+
+hashParser : Navigation.Location -> Result String Page
+hashParser location =
+    UrlParser.parse identity pageParser (String.dropLeft 1 location.hash)
+
+
+type Page
+    = Home
+    | MealPage Int
+
+
+pageParser : Parser (Page -> a) a
+pageParser =
+    oneOf
+        [ format Home (UrlParser.s "home")
+        , format MealPage (UrlParser.s "meal" </> int)
+        ]
+
+
+toHash : Page -> String
+toHash page =
+    case page of
+        Home ->
+            "#home"
+
+        MealPage id ->
+            "#meal/" ++ toString id
 
 
 
@@ -19,7 +64,10 @@ main =
 
 
 type alias Model =
-    { meals : List Meal }
+    { page : Page
+    , meals : List Meal
+    , meal : Maybe Meal
+    }
 
 
 type alias Meal =
@@ -54,27 +102,30 @@ type alias Component =
     }
 
 
-model : Model
-model =
-    { meals =
-        [ { id = 1
-          , name = "Sufuruoka"
-          , ingredients =
-                [ { amount = 1.0
-                  , ingredient =
-                        { id = 1
-                        , name = "Litteä kana"
-                        , components =
-                            [ { value = 1.0
-                              , component = { id = 1, name = "Mako" }
-                              }
-                            ]
-                        }
-                  }
-                ]
-          }
-        ]
-    }
+initialMeals : List Meal
+initialMeals =
+    [ { id = 1
+      , name = "Sufuruoka"
+      , ingredients =
+            [ { amount = 1.0
+              , ingredient =
+                    { id = 1
+                    , name = "Litteä kana"
+                    , components =
+                        [ { value = 1.0
+                          , component = { id = 1, name = "Mako" }
+                          }
+                        ]
+                    }
+              }
+            ]
+      }
+    ]
+
+
+init : Result String Page -> ( Model, Cmd Msg )
+init result =
+    urlUpdate result (Model Home initialMeals (List.head initialMeals))
 
 
 
@@ -83,13 +134,52 @@ model =
 
 type Msg
     = NoOp
+    | ShowMeal Int
 
 
-update : Msg -> Model -> Model
+{-| Update based on messages. Cause URLs to change maybe.
+-}
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            model
+            ( model, Cmd.none )
+
+        ShowMeal id ->
+            let
+                mealPage =
+                    MealPage id
+            in
+                ( { model | page = mealPage }, (Navigation.newUrl (toHash mealPage)) )
+
+
+{-| When URL updates, change model.
+-}
+urlUpdate : Result String Page -> Model -> ( Model, Cmd Msg )
+urlUpdate result model =
+    case Debug.log "result" result of
+        Err _ ->
+            ( model, Navigation.modifyUrl (toHash Home) )
+
+        Ok ((MealPage id) as page) ->
+            ( { model
+                | page = page
+                , meal = List.head (List.filter (\x -> x.id == id) initialMeals)
+              }
+            , Cmd.none
+            )
+
+        Ok Home ->
+            ( { model | page = Home, meal = Nothing }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
